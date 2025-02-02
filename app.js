0082b2ff8091d88ca79509d85122001566643bc3
ncarrascocares <nicolascarrasco15@gmail.com>
2025-02-02 00:37:22 -0300

    Se crea la función agregar amigos y otra función para ir limpiando el input

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 // Creación del arrays para almacenar la lista de amigos
-let amigos = []
+let amigos = [];
+let dato;
+
+//PENDIENTE:Queda por validar que no se acepten valores vacios en el array
+
+// funcion
+function agregarAmigo(){
+    dato = document.getElementById("amigo").value;
+    //Llenando el array
+    amigos.push(dato);
+    limpiar();
+    console.log(amigos);
+}
+
+//Esta función quedo operativa
+function limpiar(){
+    document.getElementById("amigo").value = "";
+}
