92d3cd3cd04e28838b75480c583e4c52c68f526a
ncarrascocares <nicolascarrasco15@gmail.com>
2025-02-01 23:29:24 -0300

    Creación del array para el almacenamiento de la lista

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Creación del arrays para almacenar la lista de amigos
+let amigos = []
