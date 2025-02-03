17dd48f734760184904dbc0649e6d9aae36fe485
ncarrascocares <nicolascarrasco15@gmail.com>
2025-02-03 17:03:54 -0300

    Corrección de la función sortearAmigo para que sea mas legible

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 // Creación del arrays para almacenar la lista de amigos
-let amigos = [];
-let dato;
-
-//PENDIENTE:Queda por validar que no se acepten valores vacios en el array
+var amigos = [];
+var dato;
+var mensaje = document.getElementById("resultado");
 
 // funcion
 function agregarAmigo(){
     dato = document.getElementById("amigo").value;
-    //Validando que los datos no vengan vacios
-    //Llenando el array
     validaDatos(dato)
     limpiar();
-    console.log(amigos);
+    //console.log(amigos);
 }
 
 function sortearAmigo(){
     elementosArrays = amigos.length;
-    elementosArrays == 0 ? alert("No se han ingresado amigos") : console.log(amigos[Math.floor(Math.random() * elementosArrays)]);
+    var aleatorio = Math.floor(Math.random() * elementosArrays)
+    console.log(mensaje)
+    elementosArrays == 0 ? alert("No se han ingresado amigos") : mensaje.innerText = amigos[aleatorio];
 }
 
 //Esta función quedo operativa
